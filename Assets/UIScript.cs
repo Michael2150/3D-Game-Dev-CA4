2708f24978c992162f5350893d9abf6f305b89ed
Michael2150 <52236517+Michael2150@users.noreply.github.com>
2022-05-07 00:18:04 +0100

    Scene Switching

--- Assets/UIScript.cs
+++ Assets/UIScript.cs
@@ -1,8 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UIScript : MonoBehaviour
 {
-    
+    public void StartGame()
+    {
+        GameManager.Instance.loadLevel("ExternalScene");
+    }
+
+    public void GameOver()
+    {
+        GameManager.Instance.loadLevel("GameOverScene");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
 }
