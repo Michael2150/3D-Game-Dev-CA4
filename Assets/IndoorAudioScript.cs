1e42043c18ee6dbad852419b415501b2ab734360
Michael2150 <52236517+Michael2150@users.noreply.github.com>
2022-05-10 12:40:48 +0100

    Indoor sound

---
+++ Assets/IndoorAudioScript.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndoorAudioScript : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    private BoxCollider _boxCollider;
+    private float volume = 0.15f;
+    private float fadeSpeed = 0.5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _boxCollider = GetComponent<BoxCollider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Fade in the audio source
+        _audioSource.volume = 0;
+        _audioSource.Play();
+        StartCoroutine(FadeIn());
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        //Fade out the audio source
+        StartCoroutine(FadeOut());
+    }
+    
+    private IEnumerator FadeIn()
+    {
+        while (_audioSource.volume < volume)
+        {
+            _audioSource.volume += fadeSpeed * Time.deltaTime;
+            yield return null;
+        }
+    }
+    
+    private IEnumerator FadeOut()
+    {
+        while (_audioSource.volume > 0)
+        {
+            _audioSource.volume -= fadeSpeed * Time.deltaTime;
+            yield return null;
+        }
+    }
+}
